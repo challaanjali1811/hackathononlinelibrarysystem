4355b5247466fe1bf9d680debceb966ee3871bb2
Renuka Challa <challarenuka20@gmail.com>
2023-10-27 17:51:28 +0530

    first commit

---
+++ app/src/main/java/com/improve10x/hackathononlinelibrarymanagementsystem/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.improve10x.hackathononlinelibrarymanagementsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
